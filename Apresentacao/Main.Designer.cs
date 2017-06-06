b506940e03d51a3223efa9bd7f05a0a728532da6
rwguerra <rwguerra@gmail.com>
2017-06-06 00:21:17 -0300

    Modificações interface

--- Apresentacao/Main.Designer.cs
+++ Apresentacao/Main.Designer.cs
@@ -60,13 +60,6 @@
             this.ribbonUpDown12 = new System.Windows.Forms.RibbonUpDown();
             this.ribbonUpDown13 = new System.Windows.Forms.RibbonUpDown();
             this.ribbonSeparator4 = new System.Windows.Forms.RibbonSeparator();
-            this.ribbonCheckBox1 = new System.Windows.Forms.RibbonCheckBox();
-            this.ribbonCheckBox2 = new System.Windows.Forms.RibbonCheckBox();
-            this.ribbonCheckBox3 = new System.Windows.Forms.RibbonCheckBox();
-            this.ribbonSeparator1 = new System.Windows.Forms.RibbonSeparator();
-            this.ribbonCheckBox4 = new System.Windows.Forms.RibbonCheckBox();
-            this.ribbonCheckBox5 = new System.Windows.Forms.RibbonCheckBox();
-            this.ribbonCheckBox6 = new System.Windows.Forms.RibbonCheckBox();
             this.ribbonPanel11 = new System.Windows.Forms.RibbonPanel();
             this.ribbonButton5 = new System.Windows.Forms.RibbonButton();
             this.ribbonTab2 = new System.Windows.Forms.RibbonTab();
@@ -88,12 +81,6 @@
             this.ribbonButton11 = new System.Windows.Forms.RibbonButton();
             this.plot = new OxyPlot.WindowsForms.PlotView();
             this.ribbonOrbOptionButton1 = new System.Windows.Forms.RibbonOrbOptionButton();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ribbonOrbMenuItem3
@@ -101,19 +88,21 @@
             this.ribbonOrbMenuItem3.DropDownArrowDirection = System.Windows.Forms.RibbonArrowDirection.Left;
             this.ribbonOrbMenuItem3.Image = ((System.Drawing.Image)(resources.GetObject("ribbonOrbMenuItem3.Image")));
             this.ribbonOrbMenuItem3.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonOrbMenuItem3.SmallImage")));
-            this.ribbonOrbMenuItem3.Text = "ribbonOrbMenuItem3";
+            this.ribbonOrbMenuItem3.Text = "Opções de conta";
             // 
             // ribbonOrbRecentItem1
             // 
             this.ribbonOrbRecentItem1.Image = ((System.Drawing.Image)(resources.GetObject("ribbonOrbRecentItem1.Image")));
             this.ribbonOrbRecentItem1.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonOrbRecentItem1.SmallImage")));
-            this.ribbonOrbRecentItem1.Text = "ribbonOrbRecentItem1";
+            this.ribbonOrbRecentItem1.Text = "Logar";
+            this.ribbonOrbRecentItem1.Click += new System.EventHandler(this.ribbonOrbRecentItem1_Click);
             // 
             // ribbonOrbRecentItem2
             // 
             this.ribbonOrbRecentItem2.Image = ((System.Drawing.Image)(resources.GetObject("ribbonOrbRecentItem2.Image")));
             this.ribbonOrbRecentItem2.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonOrbRecentItem2.SmallImage")));
-            this.ribbonOrbRecentItem2.Text = "ribbonOrbRecentItem2";
+            this.ribbonOrbRecentItem2.Text = "Nova conta";
+            this.ribbonOrbRecentItem2.Click += new System.EventHandler(this.ribbonOrbRecentItem2_Click);
             // 
             // ribbonButton2
             // 
@@ -128,6 +117,7 @@
             this.ribbonButton3.MaxSizeMode = System.Windows.Forms.RibbonElementSizeMode.Compact;
             this.ribbonButton3.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton3.SmallImage")));
             this.ribbonButton3.Text = "ribbonButton3";
+            this.ribbonButton3.Click += new System.EventHandler(this.ribbonButton3_Click);
             // 
             // ribbonButton4
             // 
@@ -154,6 +144,7 @@
             this.ribbonButton1.Image = ((System.Drawing.Image)(resources.GetObject("ribbonButton1.Image")));
             this.ribbonButton1.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton1.SmallImage")));
             this.ribbonButton1.Text = "INSERIR DADOS";
+            this.ribbonButton1.Click += new System.EventHandler(this.ribbonButton1_Click);
             // 
             // ribbonPanel2
             // 
@@ -164,6 +155,7 @@
             // 
             // ribbonTextBox1
             // 
+            this.ribbonTextBox1.Enabled = false;
             this.ribbonTextBox1.LabelWidth = 110;
             this.ribbonTextBox1.Text = "Nome Previsão:";
             this.ribbonTextBox1.TextBoxText = "";
@@ -176,6 +168,7 @@
             this.ribbonComboBox1.DropDownItems.Add(this.ribbonLabel3);
             this.ribbonComboBox1.DropDownItems.Add(this.ribbonLabel4);
             this.ribbonComboBox1.DropDownItems.Add(this.ribbonLabel5);
+            this.ribbonComboBox1.Enabled = false;
             this.ribbonComboBox1.LabelWidth = 110;
             this.ribbonComboBox1.Text = "Série temporal em:";
             this.ribbonComboBox1.TextBoxText = "";
@@ -203,6 +196,7 @@
             // 
             // ribbonTextBox2
             // 
+            this.ribbonTextBox2.Enabled = false;
             this.ribbonTextBox2.LabelWidth = 110;
             this.ribbonTextBox2.Text = "Grandeza:";
             this.ribbonTextBox2.TextBoxText = "";
@@ -221,13 +215,6 @@
             this.ribbonPanel3.Items.Add(this.ribbonUpDown12);
             this.ribbonPanel3.Items.Add(this.ribbonUpDown13);
             this.ribbonPanel3.Items.Add(this.ribbonSeparator4);
-            this.ribbonPanel3.Items.Add(this.ribbonCheckBox1);
-            this.ribbonPanel3.Items.Add(this.ribbonCheckBox2);
-            this.ribbonPanel3.Items.Add(this.ribbonCheckBox3);
-            this.ribbonPanel3.Items.Add(this.ribbonSeparator1);
-            this.ribbonPanel3.Items.Add(this.ribbonCheckBox4);
-            this.ribbonPanel3.Items.Add(this.ribbonCheckBox5);
-            this.ribbonPanel3.Items.Add(this.ribbonCheckBox6);
             this.ribbonPanel3.Text = "Configuração";
             // 
             // ribbonUpDown2
@@ -240,6 +227,7 @@
             // 
             // ribbonUpDown3
             // 
+            this.ribbonUpDown3.Enabled = false;
             this.ribbonUpDown3.LabelWidth = 80;
             this.ribbonUpDown3.Text = "MS Grandeza:";
             this.ribbonUpDown3.TextBoxText = "";
@@ -247,6 +235,7 @@
             // 
             // ribbonUpDown4
             // 
+            this.ribbonUpDown4.Enabled = false;
             this.ribbonUpDown4.LabelWidth = 80;
             this.ribbonUpDown4.Text = "MOD MS";
             this.ribbonUpDown4.TextBoxText = "";
@@ -254,6 +243,7 @@
             // 
             // ribbonUpDown8
             // 
+            this.ribbonUpDown8.Enabled = false;
             this.ribbonUpDown8.LabelWidth = 80;
             this.ribbonUpDown8.Text = "MS Tempo";
             this.ribbonUpDown8.TextBoxText = "";
@@ -261,6 +251,7 @@
             // 
             // ribbonUpDown9
             // 
+            this.ribbonUpDown9.Enabled = false;
             this.ribbonUpDown9.LabelWidth = 80;
             this.ribbonUpDown9.Text = "MS Grandeza";
             this.ribbonUpDown9.TextBoxText = "";
@@ -268,6 +259,7 @@
             // 
             // ribbonUpDown10
             // 
+            this.ribbonUpDown10.Enabled = false;
             this.ribbonUpDown10.LabelWidth = 80;
             this.ribbonUpDown10.Text = "MOD MS";
             this.ribbonUpDown10.TextBoxText = "";
@@ -275,6 +267,7 @@
             // 
             // ribbonUpDown11
             // 
+            this.ribbonUpDown11.Enabled = false;
             this.ribbonUpDown11.LabelWidth = 110;
             this.ribbonUpDown11.Text = "Janela de Previsão:";
             this.ribbonUpDown11.TextBoxText = "";
@@ -282,6 +275,7 @@
             // 
             // ribbonUpDown12
             // 
+            this.ribbonUpDown12.Enabled = false;
             this.ribbonUpDown12.LabelWidth = 110;
             this.ribbonUpDown12.Text = "Config.";
             this.ribbonUpDown12.TextBoxText = "";
@@ -289,34 +283,11 @@
             // 
             // ribbonUpDown13
             // 
+            this.ribbonUpDown13.Enabled = false;
             this.ribbonUpDown13.LabelWidth = 110;
             this.ribbonUpDown13.Text = "Config.";
             this.ribbonUpDown13.TextBoxText = "";
             this.ribbonUpDown13.TextBoxWidth = 50;
-            // 
-            // ribbonCheckBox1
-            // 
-            this.ribbonCheckBox1.Text = "";
-            // 
-            // ribbonCheckBox2
-            // 
-            this.ribbonCheckBox2.Text = "";
-            // 
-            // ribbonCheckBox3
-            // 
-            this.ribbonCheckBox3.Text = "";
-            // 
-            // ribbonCheckBox4
-            // 
-            this.ribbonCheckBox4.Text = "";
-            // 
-            // ribbonCheckBox5
-            // 
-            this.ribbonCheckBox5.Text = "";
-            // 
-            // ribbonCheckBox6
-            // 
-            this.ribbonCheckBox6.Text = "";
             // 
             // ribbonPanel11
             // 
@@ -328,6 +299,7 @@
             this.ribbonButton5.Image = ((System.Drawing.Image)(resources.GetObject("ribbonButton5.Image")));
             this.ribbonButton5.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton5.SmallImage")));
             this.ribbonButton5.Text = "PREVER DADOS";
+            this.ribbonButton5.Click += new System.EventHandler(this.ribbonButton5_Click);
             // 
             // ribbonTab2
             // 
@@ -425,7 +397,7 @@
             this.ribbonMain.QuickAcessToolbar.Items.Add(this.ribbonButton4);
             this.ribbonMain.QuickAcessToolbar.Items.Add(this.ribbonButton11);
             this.ribbonMain.RibbonTabFont = new System.Drawing.Font("Trebuchet MS", 9F);
-            this.ribbonMain.Size = new System.Drawing.Size(1130, 147);
+            this.ribbonMain.Size = new System.Drawing.Size(868, 147);
             this.ribbonMain.TabIndex = 0;
             this.ribbonMain.Tabs.Add(this.ribbonTab1);
             this.ribbonMain.Tabs.Add(this.ribbonTab2);
@@ -443,14 +415,12 @@
             // 
             // plot
             // 
-            this.plot.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.plot.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.plot.Location = new System.Drawing.Point(58, 240);
+            this.plot.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.plot.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.plot.Location = new System.Drawing.Point(0, 147);
             this.plot.Name = "plot";
             this.plot.PanCursor = System.Windows.Forms.Cursors.Hand;
-            this.plot.Size = new System.Drawing.Size(1011, 233);
+            this.plot.Size = new System.Drawing.Size(868, 348);
             this.plot.TabIndex = 27;
             this.plot.Text = "plot2";
             this.plot.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
@@ -463,82 +433,21 @@
             this.ribbonOrbOptionButton1.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonOrbOptionButton1.SmallImage")));
             this.ribbonOrbOptionButton1.Text = "ribbonOrbOptionButton1";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(183, 178);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 28;
-            this.button1.Text = "Logar";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(292, 178);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 29;
-            this.button2.Text = "Novo Login";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(407, 186);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 30;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(404, 170);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 13);
-            this.label1.TabIndex = 31;
-            this.label1.Text = "MS Variavel";
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(528, 173);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(115, 33);
-            this.button3.TabIndex = 32;
-            this.button3.Text = "PREVER";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(23, 165);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(94, 36);
-            this.button4.TabIndex = 33;
-            this.button4.Text = "Ler CSV";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click_1);
-            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1130, 485);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(868, 495);
             this.Controls.Add(this.plot);
             this.Controls.Add(this.ribbonMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Sistema de Previsão Utilizando Lógica Fuzzy - SPULF";
             this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -576,13 +485,6 @@
         private System.Windows.Forms.RibbonUpDown ribbonUpDown12;
         private System.Windows.Forms.RibbonUpDown ribbonUpDown13;
         private System.Windows.Forms.RibbonSeparator ribbonSeparator4;
-        private System.Windows.Forms.RibbonCheckBox ribbonCheckBox1;
-        private System.Windows.Forms.RibbonCheckBox ribbonCheckBox2;
-        private System.Windows.Forms.RibbonCheckBox ribbonCheckBox3;
-        private System.Windows.Forms.RibbonSeparator ribbonSeparator1;
-        private System.Windows.Forms.RibbonCheckBox ribbonCheckBox4;
-        private System.Windows.Forms.RibbonCheckBox ribbonCheckBox5;
-        private System.Windows.Forms.RibbonCheckBox ribbonCheckBox6;
         private System.Windows.Forms.RibbonPanel ribbonPanel11;
         private System.Windows.Forms.RibbonButton ribbonButton5;
         private System.Windows.Forms.RibbonUpDown ribbonUpDown5;
@@ -605,11 +507,5 @@
         private System.Windows.Forms.RibbonLabel ribbonLabel4;
         private System.Windows.Forms.RibbonLabel ribbonLabel5;
         private System.Windows.Forms.RibbonButton ribbonButton11;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
     }
 }
--- Apresentacao/Main.cs
+++ Apresentacao/Main.cs
@@ -9,6 +9,8 @@ using System.Windows.Forms;
 using ObjetoTransferencia;
 using Negocios;
 using System.IO;
+using OxyPlot;
+using OxyPlot.Series;
 
 namespace Apresentacao
 {
@@ -19,6 +21,9 @@ namespace Apresentacao
         public Main()
         {
             InitializeComponent();
+            var myModel = new PlotModel { Title = "Exemplo" };
+            myModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+            this.plot.Model = myModel;
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -38,7 +43,7 @@ namespace Apresentacao
 
             // CONFIGURAÇÃO VARIAVEL LINGUISTICA
 
-            int qntMSVariavel = Convert.ToInt32(textBox1.Text);
+            int qntMSVariavel = Convert.ToInt32(ribbonUpDown2);
 
             List<VariavelLinguistica> _ListaVariaveis = new List<VariavelLinguistica>();
 
@@ -121,7 +126,74 @@ namespace Apresentacao
 
         private void button4_Click_1(object sender, EventArgs e)
         {
+            
+        }
+
+        private void ribbonButton1_Click(object sender, EventArgs e)
+        {
             LerCSV();
+        }
+
+        private void ribbonButton3_Click(object sender, EventArgs e)
+        {
+            View_Login view_Login = new View_Login();
+            view_Login.ShowDialog();
+        }
+
+        private void ribbonOrbRecentItem1_Click(object sender, EventArgs e)
+        {
+            View_Login view_Login = new View_Login();
+            view_Login.ShowDialog();
+        }
+
+        private void ribbonOrbRecentItem2_Click(object sender, EventArgs e)
+        {
+            ViewCadastro viewCadastro = new ViewCadastro();
+            viewCadastro.ShowDialog();
+        }
+
+        private void ribbonButton5_Click(object sender, EventArgs e)
+        {
+            // DADOOS
+
+            List<Item> listaDados = new List<Item>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                Item item = new Item();
+                item.Tempo = i;
+                item.DadosOriginais = i;
+
+                listaDados.Add(item);
+            }
+
+            // CONFIGURAÇÃO VARIAVEL LINGUISTICA
+
+            int qntMSVariavel = Convert.ToInt32(ribbonUpDown2);
+
+            List<VariavelLinguistica> _ListaVariaveis = new List<VariavelLinguistica>();
+
+            VariavelLinguistica V0 = new VariavelLinguistica();
+            V0.NomeVariavel = "Variavel0";
+            V0.QntMS = qntMSVariavel;
+            _ListaVariaveis.Add(V0);
+
+            VariavelLinguistica V1 = new VariavelLinguistica();
+            V1.NomeVariavel = "Variavel1";
+            V1.QntMS = qntMSVariavel;
+            _ListaVariaveis.Add(V1);
+
+            VariavelLinguistica V2 = new VariavelLinguistica();
+            V2.NomeVariavel = "Variavel2";
+            V2.QntMS = qntMSVariavel;
+            _ListaVariaveis.Add(V2);
+
+
+            //CONFIGURAÇÃO PREVISÃO
+
+            PrevisaoFuzzy previsaoFuzzy = new PrevisaoFuzzy();
+
+            double previsao = previsaoFuzzy.Previsao(listaDados, _ListaVariaveis);
         }
     }
 }
